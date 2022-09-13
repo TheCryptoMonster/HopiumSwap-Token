bbc4abf995eee7bdcaf5ed5238732f64419b5ed1
Crypto Monster <66889078+TheCryptoMonster@users.noreply.github.com>
2022-09-13 16:38:42 -0700

    Add files via upload

---
+++ test/HopeTokenTest.js
@@ -0,0 +1,14 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Hope token", function () {
+  it("Should return owner of token", async function () {
+    const [owner]=await ethers.getSigners();
+    const HopeToken = await ethers.getContractFactory("HopeToken");
+    const hopeToken = await HopeToken.deploy();
+    await hopeToken.deployed();
+    const owneraddress= await hopeToken.owner();
+
+    expect(owneraddress).to.equal(owner.address);
+  });
+});
